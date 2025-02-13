6fd9e3d85289fd6ca0c2831d15b7e09f61547f79
vladik-didyk <62429920+vladik-Didyk@users.noreply.github.com>
2025-02-13 10:31:48 -0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -54,6 +54,42 @@ export const projects = [
       { name: "Geological Modeling", color: "pink-text-gradient" },
     ],
     image: mhft,
-    source_code_link: "https://github.com/resource-estimation-ai",
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Resource Estimation AI",
+    description:
+      "AI model that uses regression techniques to estimate mineral resources and predict deposit locations.",
+    tags: [
+      { name: "Machine Learning", color: "blue-text-gradient" },
+      { name: "Predictive Analytics", color: "green-text-gradient" },
+      { name: "Geological Modeling", color: "pink-text-gradient" },
+    ],
+    image: mhft,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Resource Estimation AI",
+    description:
+      "AI model that uses regression techniques to estimate mineral resources and predict deposit locations.",
+    tags: [
+      { name: "Machine Learning", color: "blue-text-gradient" },
+      { name: "Predictive Analytics", color: "green-text-gradient" },
+      { name: "Geological Modeling", color: "pink-text-gradient" },
+    ],
+    image: mhft,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Resource Estimation AI",
+    description:
+      "AI model that uses regression techniques to estimate mineral resources and predict deposit locations.",
+    tags: [
+      { name: "Machine Learning", color: "blue-text-gradient" },
+      { name: "Predictive Analytics", color: "green-text-gradient" },
+      { name: "Geological Modeling", color: "pink-text-gradient" },
+    ],
+    image: mhft,
+    source_code_link: "https://github.com/",
   }
 ];
