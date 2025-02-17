157dee3dae1e4a2a44fc4c00c85c318065c83a44
Vlad Didyk <depodart@gmail.com>
2025-02-17 13:32:58 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
         <div className={appStyles.heroSection}>
           <Navbar />
           <Hero />
+          <StarsCanvas />
         </div>
         <About />
         <Experience />
