be0af7b363caa276838ac58da0b69f7a4bcfd46a
cirojdiaz <40615809+cirojdiaz@users.noreply.github.com>
2025-02-16 22:44:43 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,73 +4,48 @@ module.exports = {
   darkMode: 'class',
   safelist: [
     {
-      // This pattern matches any bg, text, or border class with one of the hex codes;
-      // the "dark" variant is also generated.
-      pattern: /^(bg|text|border)-\[\#(?:FFFFFF|F1F1F1|050816|1d1836|232631|E5E7EB|374151|4B5563|dfd9ff|6366F1|4F46E5|4338CA|915EFF|EC4899|A855F7|00cea8|ff6b6b|007bff|ffc107|fd7e14)\]$/,
+      pattern: /^(bg|text|border)-\[\#(?:4C2B1C|CB540B|FFC409|FFFFFF)\]$/,
       variants: ['dark'],
     },
     {
-      pattern: /^(bg|text|border)-\[\#(?:FFFFFF|F1F1F1|050816|1d1836|232631|E5E7EB|374151|4B5563|dfd9ff|6366F1|4F46E5|4338CA|915EFF|EC4899|A855F7|00cea8|ff6b6b|007bff|ffc107|fd7e14)\]$/,
+      pattern: /^(bg|text|border)-\[\#(?:4C2B1C|CB540B|FFC409|FFFFFF)\]$/,
     },
   ],
   theme: {
     extend: {
       colors: {
-        'new-background-primary-dark': '#4c2b1c',
-        'new-background-secondary-dark': '#cb540b',
-        'new-background-tertiary-dark': '#ffc409',
-        'new-background-primary-light': '#FFFFFF',
+        'primary-dark': '#110A07',
+        'secondary-dark': '#CB540B',
+        'tertiary-dark': '#FFC409',
+        'primary-light': '#FFFFFF',
         
         // Background colors
-        // 'background-primary-dark': '#4c2b1c',
-        // 'background-secondary-dark': '#cb540b',
-        // 'background-tertiary-dark': '#ffc409',
-        // 'background-primary-light': '#FFFFFF',
-
-        'background-primary-dark': '#050816',
-        'background-secondary-dark': '#1d1836',
-        'background-tertiary-dark': '#232631',
+        'background-primary-dark': '#110A07',
+        'background-secondary-dark': '#CB540B',
+        'background-tertiary-dark': '#FFC409',
         'background-primary-light': '#FFFFFF',
-        'background-secondary-light': '#F1F1F1',
-        'background-tertiary-light': '#E5E7EB',
-        // Text colors (for light and dark modes; note that the dark "primary" is white)
-        'primary-dark': '#FFFFFF',
-        'secondary-dark': '#F1F1F1',
-        'tertiary-dark': '#dfd9ff',
-        'primary-light': '#1d1836',
-        'secondary-light': '#374151',
-        'tertiary-light': '#4B5563',
-        // Accent colors
-        'indigo-light': '#6366F1',
-        'indigo-medium': '#4F46E5',
-        'indigo-dark': '#4338CA',
-        'purple': '#915EFF',
-        // Special & additional accent colors
-        'hotPink': '#EC4899',
-        'lavender': '#A855F7',
-        'green': '#00cea8',
-        'blue': '#007bff',
-        'red': '#ff6b6b',
-        'yellow': '#ffc107',
-        'orange': '#fd7e14',
+
+        // Text colors
+        'text-primary-dark': '#FFFFFF',
+        'text-secondary-dark': '#FFFFFF',
+        'text-tertiary-dark': '#FFFFFF',
+        'text-primary-light': '#FFFFFF',
+
         // Common colors
         'common-primary': '#FFFFFF',
-        'common-secondary': '#F1F1F1',
-        'common-tertiary': '#E5E7EB',
-        'common-quaternary': '#1d1836',
-        // Other defaults
-        'primary': '#050816',
-        'secondary': '#aaa6c3',
-        'tertiary': '#151030',
+        // 'common-secondary': '#CB540B',
+        'common-secondary': '#FFFFFF',
+        // 'common-tertiary': '#FFC409',
+        'common-tertiary': '#FFFFFF',
       },
       boxShadow: {
-        card: '0 35px 120px -15px #211e35',
+        card: '0 35px 120px -15px #110A07',
       },
       screens: {
         xs: '450px',
       },
       backgroundImage: {
-        'hero-pattern': "url('/herobg.png')",
+        // 'hero-pattern': "url('/herobg.png')",
       },
     },
   },
