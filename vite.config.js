5e2d0901ec334701a0909b20cab8cda4d324ae81
Vlad Didyk <depodart@gmail.com>
2025-02-19 16:19:46 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
-  plugins: [react()],
+
   base: "/DepoDart.ai/",
   server: {
     port: 3000,
     host: true,
   },
+  css: {
+    postcss: {
+      plugins: [tailwindcss(), autoprefixer()],
+    },
+  },
+  plugins: [react()],
 });
