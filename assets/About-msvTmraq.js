6d374ba2fc862ac092ce2d0f07820b8c4fe24cda
Vlad Didyk <depodart@gmail.com>
2025-03-03 09:36:23 -0500

    Updates

--- assets/About-msvTmraq.js
+++ assets/About-msvTmraq.js
@@ -1,4 +1,4 @@
-import{j as a,m as e,L as l}from"./index-D9HlWSXD.js";import{A as d}from"./index-Cs1P_Aa4.js";import{c as i,B as c}from"./brain-circuit-BVOXdjaL.js";/**
+import{j as a,m as e,L as l}from"./index-CoK86Wvc.js";import{A as d}from"./index-CikWesJU.js";import{c as i,B as c}from"./brain-circuit-D3DqwiPj.js";/**
  * @license lucide-react v0.476.0 - ISC
  *
  * This source code is licensed under the ISC license.
